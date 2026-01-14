a1de60e1a1f101480cc917ba0a7f95ad2d835726
Pam Selle <pamela.selle@gmail.com>
2026-01-14 12:08:07 -0500

    Link up and show UI for token

--- src/components/ArtCard.tsx
+++ src/components/ArtCard.tsx
@@ -2,21 +2,26 @@ import {
   useGetCollectionAddress,
   useGetGalleryAddress,
   useGetOwner,
+  useGetTokenUri,
 } from "../hooks/useNftCollection";
 import styles from "./styles/ArtCard.module.css";
 
 const ArtCard: React.FC<{ tokenId: number }> = ({ tokenId }) => {
   const { data: galleryAddress } = useGetGalleryAddress();
-  console.log("Gallery Address:", galleryAddress);
 
   const { data: collectionAddress, isLoading: isLoadingAddress } =
     useGetCollectionAddress();
-  console.log("Collection Address:", collectionAddress);
   const { data: owner, isLoading: isLoadingOwner } = useGetOwner(
     tokenId,
     collectionAddress,
     galleryAddress,
   );
+
+  const { data: tokenURI, isLoading: isLoadingTokenUri } = useGetTokenUri(
+    tokenId,
+    collectionAddress,
+  );
+
   const leftPadTokenId = String(tokenId).padStart(2, "0");
 
   const isLoading = isLoadingAddress || isLoadingOwner;
@@ -24,13 +29,21 @@ const ArtCard: React.FC<{ tokenId: number }> = ({ tokenId }) => {
 
   return (
     <div className={styles.artCard}>
-      <h3>
-        art piece #{tokenId}, owned by {displayOwner ?? "Loading..."}
-      </h3>
+      <h2>Square #{tokenId}</h2>
       <img
         src={`./art/${leftPadTokenId}-squares.png`}
         alt={`art piece #${tokenId}`}
       />
+      <div className={styles.artCardInfo}>
+        <p>Owned by {displayOwner ?? "Loading..."}</p>
+        {!isLoadingTokenUri && tokenURI && (
+          <p className={styles.tokenUri}>
+            <a href={tokenURI} target="_blank" rel="noopener noreferrer">
+              Token URI
+            </a>
+          </p>
+        )}
+      </div>
     </div>
   );
 };
