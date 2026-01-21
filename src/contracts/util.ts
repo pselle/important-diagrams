b8f09ea0d657893ccc99731ff4b91292a39b28a1
Pam Selle <pamela.selle@gmail.com>
2026-01-21 11:34:11 -0500

    Revert "Change prefix to VITE_ bc of issues in development"
    This reverts commit 422ad8139e3b99c5a255370a7de653b4a2355d9a.

--- src/contracts/util.ts
+++ src/contracts/util.ts
@@ -15,38 +15,34 @@ type Network = {
 };
 
 const envSchema = z.object({
-  VITE_STELLAR_NETWORK: z.enum([
+  PUBLIC_STELLAR_NETWORK: z.enum([
     "PUBLIC",
     "FUTURENET",
     "TESTNET",
     "LOCAL",
     "STANDALONE", // deprecated in favor of LOCAL
   ] as const),
-  VITE_STELLAR_NETWORK_PASSPHRASE: z.nativeEnum(WalletNetwork),
-  VITE_STELLAR_RPC_URL: z.string(),
-  VITE_STELLAR_HORIZON_URL: z.string(),
+  PUBLIC_STELLAR_NETWORK_PASSPHRASE: z.nativeEnum(WalletNetwork),
+  PUBLIC_STELLAR_RPC_URL: z.string(),
+  PUBLIC_STELLAR_HORIZON_URL: z.string(),
 });
 
 const parsed = envSchema.safeParse(import.meta.env);
-console.log(import.meta.env);
-console.log(parsed);
 
 const env: z.infer<typeof envSchema> = parsed.success
   ? parsed.data
   : {
-      VITE_STELLAR_NETWORK: "LOCAL",
-      VITE_STELLAR_NETWORK_PASSPHRASE: WalletNetwork.STANDALONE,
-      VITE_STELLAR_RPC_URL: "http://localhost:8000/rpc",
-      VITE_STELLAR_HORIZON_URL: "http://localhost:8000",
+      PUBLIC_STELLAR_NETWORK: "LOCAL",
+      PUBLIC_STELLAR_NETWORK_PASSPHRASE: WalletNetwork.STANDALONE,
+      PUBLIC_STELLAR_RPC_URL: "http://localhost:8000/rpc",
+      PUBLIC_STELLAR_HORIZON_URL: "http://localhost:8000",
     };
 
-console.log(env);
-
 export const stellarNetwork =
-  env.VITE_STELLAR_NETWORK === "STANDALONE"
+  env.PUBLIC_STELLAR_NETWORK === "STANDALONE"
     ? "LOCAL"
-    : env.VITE_STELLAR_NETWORK;
-export const networkPassphrase = env.VITE_STELLAR_NETWORK_PASSPHRASE;
+    : env.PUBLIC_STELLAR_NETWORK;
+export const networkPassphrase = env.PUBLIC_STELLAR_NETWORK_PASSPHRASE;
 
 const stellarEncode = (str: string) => {
   return str.replace(/\//g, "//").replace(/;/g, "/;");
@@ -68,10 +64,9 @@ export const labPrefix = () => {
 };
 
 // NOTE: needs to be exported for contract files in this directory
-export const rpcUrl = env.VITE_STELLAR_RPC_URL;
-console.log("Using RPC URL:", rpcUrl);
+export const rpcUrl = env.PUBLIC_STELLAR_RPC_URL;
 
-export const horizonUrl = env.VITE_STELLAR_HORIZON_URL;
+export const horizonUrl = env.PUBLIC_STELLAR_HORIZON_URL;
 
 const networkToId = (network: string): NetworkType => {
   switch (network) {
--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig(() => {
     define: {
       global: "window",
     },
+    envPrefix: "PUBLIC_",
     server: {
       proxy: {
         "/friendbot": {
